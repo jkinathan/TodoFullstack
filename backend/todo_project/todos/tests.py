64fc3fa0c3b9e782b9c5f1a3248c4ae68411acdb
jkinobe <jkinobe@gmail.com>
2020-06-15 16:05:24 +0300

    test backend APIs corrected tested and passed

--- backend/todo_project/todos/tests.py
+++ backend/todo_project/todos/tests.py
@@ -11,10 +11,10 @@ class TodoModelTest(TestCase):
     
     def test_title_content(self):
         todo = Todo.objects.get(id=1)
-        expected_object_name = f'{todo.title}'
+        expected_object_name = '{todo.title}'
         self.assertEquals(expected_object_name, 'first todo')
         
     def test_body_content(self):
         todo = Todo.objects.get(id=1)
-        expected_object_name = f'{todo.body}'
+        expected_object_name = '{todo.body}'
         self.assertEquals(expected_object_name, 'a body here')
