25683f86373abf4f7224378acdbd87105c7b30b8
jkinobe <jkinobe@gmail.com>
2020-06-15 15:33:13 +0300

    added ListAPIView and RetrieveAPIView from rest_frameworks' generics

---
+++ backend/todo_project/todos/serializers.py
@@ -0,0 +1,8 @@
+#todo serializers
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id','title','body',)
--- backend/todo_project/todos/views.py
+++ backend/todo_project/todos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import TodoSerializer
+from .models import Todo
 # Create your views here.
+
+class ListTodo(generics.ListAPIView):
+    querysey = Todo.objects.all()
+    serializer_class = TodoSerializer
+    
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
